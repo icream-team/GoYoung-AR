f48241740aaf4a3e6c30879b1bd739956bc95eb0
YOUNGKIM1234 <40260186+YOUNGKIM1234@users.noreply.github.com>
2018-11-23 04:13:06 +0900

    .

---
+++ Assets/Script/HelpButtonClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HelpButtonClick : MonoBehaviour {
+    private GameObject help;
+
+    public void Onclick()
+    {
+        help.SetActive(true);
+    }
+    void Start()
+    {
+        help = GameObject.FindWithTag("Help");
+    }
+
+}
---
+++ Assets/Script/SetButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetButton : MonoBehaviour
+{
+    private GameObject set;
+
+    public void Onclick()
+    {
+        set.SetActive(true);
+    }
+    void Start()
+    {
+        set = GameObject.FindWithTag("Set");
+    }
+
+}
